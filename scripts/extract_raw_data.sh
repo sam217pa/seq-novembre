f0b041824357ce319623e54033a7bd9ae63e6891
sam217pa <samuel.barreto8@gmail.com>
2015-11-02 12:47:06 +0100

    workflow en place

---
+++ scripts/extract_raw_data.sh
@@ -0,0 +1,11 @@
+cd ./data
+# extraction des données brutes
+unzip raw_seq_nvbr/1369607.zip
+unzip raw_seq_nvbr/1369628.zip
+# déplace tout les fichiers dans des dossiers adaptés 
+find . -name "*.fas" -exec mv -i {} -t ./fasta/ \;
+find . -name "*.ab1" -exec mv -i {} -t ./spectrograms/ \;
+find . -name "*.seq" -exec mv -i {} -t ./seq/ \;
+# déplace le contenu du dossier inutile dans le présent dossier
+mv 1369628/* ./
+rm -r 1369628 # supprime le dossier
