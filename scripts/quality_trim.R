0639215a6d3eeabd9ddbf21f0b2482ba5f06aa97
sam217pa <samuel.barreto8@gmail.com>
2015-11-16 15:14:55 +0100

    avant de modifier le script python

--- scripts/quality_trim.R
+++ scripts/quality_trim.R
@@ -1,33 +1,36 @@
 library(ShortRead)
 library(ggplot2)
-fls <- dir("./data/tmp", "*fastq$", full=TRUE)
-qaSummary <- qa(fls, type="fastq")
-browseURL(report(qaSummary))
 
-filterAndTrim <-
-    function(fl, destination = sprintf("%s-subset", fl)) {
-        ## open input stream
-        stream <- open(FastqStreamer(fl))
-        on.exit(close(stream))
+filterAndTrim <- function(fl, destination = sprintf("%s-subset", fl)) {
+    ## open input stream
+    stream <- open(FastqStreamer(fl))
+    on.exit(close(stream))
 
-        repeat {
-            ## input chunk
-            fq <- yield(stream)
-            if (length(fq) == 0)
-                break
-            ## trim and filter, e.g. reads cannot contain 'N'
-            fq <- fq[nFilter()(fq)] # see ?srFilter for predefined filters
-            ## trim as soon as 2 of 5 ncleotides has quality encoding less
-            ## than "4" (phred score 20)
-            fq <- trimTailw(fq, 2, "4", 2)
-            ## drop reads that are less than 36 nt
-            fq <- fq[width(fq) >= 36]
+    repeat {
+        ## input chunk
+        fq <- yield(stream)
+        if (length(fq) == 0)
+            break
+        ## trim and filter, e.g. reads cannot contain 'N'
+        fq <- fq[nFilter()(fq)] # see ?srFilter for predefined filters
+        ## trim as soon as 2 of 5 ncleotides has quality encoding less
+        ## than "4" (phred score 20)
+        fq <- trimTailw(fq, 2, "4", 2)
+        ## drop reads that are less than 36 nt
+        fq <- fq[width(fq) >= 36]
 
-            ## append to destination
-            writeFastq(fq, destination, "a")
-        }
+        ## append to destination
+        writeFastq(fq, destination, "a")
     }
+}
 
-fastq <- readFastq(dirPath = "./data/fastq", pattern = "*fastq$")
-quality(fastq)
+fastq <- readFastq(dirPath = "../data/fastq", pattern = "*fastq$")
 qplot(width(fastq), geom = "density")
+quality(fastq)
+qual <- PhredQuality(quality(quality(fastq)))
+qual
+myqual_mat <- matrix(charToRaw(as.character(unlist(qual))), nrow=length(qual), byrow=TRUE) # convert quality score to matrix
+at <- myqual_mat < charToRaw(as.character(PhredQuality(as.integer(qualityCutoff)))) # find positions of low quality
+letter_subject <- DNAString(paste(rep.int("N", width(seqs)[1]), collapse="")) # create a matrix of Ns
+letter <- as(Views(letter_subject, start=1, end=rowSums(at)), "DNAStringSet") # trim to length needed for each read
+injectedseqs <- replaceLetterAt(seqs, at, letter) # inject Ns at low quality positions
