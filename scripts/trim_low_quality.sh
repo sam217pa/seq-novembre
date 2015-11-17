81e4507deb25ddfe8a4896690557cc9861282c99
sam217pa <samuel.barreto8@gmail.com>
2015-11-17 09:40:37 +0100

    removed low_quality.py

---
+++ scripts/trim_low_quality.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/bash
+
+# -qtrim=rl : quality trim right and left 
+# -trimq=28 : trim if quality < 28 (sanger encoding, illumina 1.9)
+# -minlen=620 : keep only seq with length > 620, after trimming.
+
+if [[ ! -f ../data/untrimmed.fastq && ! -f ../data/trimmed.fastq ]]; then # seulement si les fichiers n'existent pas. 
+    ~/.bin/bbmap/bbduk.sh -Xmx1g -in=../data/untrimmed.fastq -out=../data/trimmed.fastq qtrim=rl trimq=28 -minlen=620
+else
+    rm ../data/trimmed.fastq
+    rm ../data/untrimmed.fastq
+    ./quality_check # assemble tous les fichiers .fastq de novo
+    ~/.bin/bbmap/bbduk.sh -Xmx1g -in=../data/untrimmed.fastq -out=../data/trimmed.fastq qtrim=rl trimq=28 -minlen=620
+fi
