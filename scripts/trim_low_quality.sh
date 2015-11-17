46a738b63f1b17694c81a20b5d6007b82ce0c0bf
sam217pa <samuel.barreto8@gmail.com>
2015-11-17 09:51:59 +0100

    quality trim done.

--- scripts/trim_low_quality.sh
+++ scripts/trim_low_quality.sh
@@ -4,7 +4,7 @@
 # -trimq=28 : trim if quality < 28 (sanger encoding, illumina 1.9)
 # -minlen=620 : keep only seq with length > 620, after trimming.
 
-if [[ ! -f ../data/untrimmed.fastq && ! -f ../data/trimmed.fastq ]]; then # seulement si les fichiers n'existent pas. 
+if [[ -f ../data/untrimmed.fastq && ! -f ../data/trimmed.fastq ]]; then # seulement si les fichiers n'existent pas. 
     ~/.bin/bbmap/bbduk.sh -Xmx1g -in=../data/untrimmed.fastq -out=../data/trimmed.fastq qtrim=rl trimq=28 -minlen=620
 else
     rm ../data/trimmed.fastq
@@ -12,3 +12,6 @@ else
     ./quality_check # assemble tous les fichiers .fastq de novo
     ~/.bin/bbmap/bbduk.sh -Xmx1g -in=../data/untrimmed.fastq -out=../data/trimmed.fastq qtrim=rl trimq=28 -minlen=620
 fi
+
+fastqc trimmed.fastq -o ./tmp
+mv ./tmp/trimmed_fastqc.html ../analysis
