045a74581a9935fb200de982a45848349ca0423d
sam217pa <samuel.barreto8@gmail.com>
2015-11-18 06:37:45 +0100

    switched to ssaha2 for snp calling

---
+++ scripts/variantCallerSsaha2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# variant calling using ssaha2 and ssaha2SNP
+
+## alignement à la séquence de référence
+#'
+#'
+~/.bin/ssahaSNP/ssaha2 ref.fasta reads.fastq ...
+
+## polymorphism detection tool
+#'
+#'
+~/.bin/ssahaSNP/ssaahaSNP
